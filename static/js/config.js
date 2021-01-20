1005e8adc6e6e6095b5370aafb14f13eb156cc9c
ALLCAPs788 <capspfifer@gmail.com>
2021-01-20 18:00:04 -0500

    more templates

---
+++ static/js/config.js
@@ -0,0 +1,1 @@
+var API_KEY = "pk.eyJ1IjoiY3BmaWZlciIsImEiOiJja2pwdWw2bHQxMjRzMnhwZXFqaGxjZGlzIn0.8ZFEfDxq_1u4dmZDn4zJGA";
--- static/js/logic.js
+++ static/js/logic.js
@@ -1,2 +1,63 @@
 //add config.js ask teachers whether gitignore is typical for public tokens
 //more notes
+
+function createmap(quakes) {
+
+  // Create a baseMaps object to hold the lightmap layer
+  
+  L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/streets-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+  // Create an overlayMaps object to hold the bikeStations layer
+   var overlayMaps = {
+     'depth': coordinates[2] 
+   };
+
+
+
+
+// Define a map object
+var myMap = L.map("map", {
+    center: [37.09, -95.71],
+    zoom: 5,
+    layers: [streetmap, states, cities]
+  });
+  
+  // Pass our map layers into our layer control
+  // Add the layer control to the map
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap);
+
+  function createMarkers(response) {
+
+    // Pull the "stations" property off of response.data
+    var stations = response.data.stations;
+  
+    // Initialize an array to hold bike markers
+    var bikeMarkers = [];
+  
+    // Loop through the stations array
+    for (var index = 0; index < stations.length; index++) {
+      var station = stations[index];
+  
+      // For each station, create a marker and bind a popup with the station's name
+      var quakeMarker = L.marker([station.lat, station.lon])
+        .bindPopup("<h3>" + station.name + "<h3><h3>Capacity: " + station.capacity + "</h3>");
+  
+      // Add the marker to the bikeMarkers array
+      bikeMarkers.push(quakeMarker);
+    }
+  
+    // Create a layer group made from the bike markers array, pass it into the createMap function
+    createMap(L.layerGroup(bikeMarkers));
+  }
+ 
+ 
+  d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson')
+  
